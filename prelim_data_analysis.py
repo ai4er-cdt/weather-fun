b94e74387682e001e2a942db2a232ba873198c32
Michelle Wan <mwlw3@cam.ac.uk>
2019-10-19 20:20:20 +0100

    Added a function to specify home or work data file paths; added a loop to divide specific data columns by given factor and replace old columns

--- prelim_data_analysis.py
+++ prelim_data_analysis.py
@@ -31,49 +31,34 @@ data_labels = ["time", "temp_10", "humidity", "dew_point_10",
 
 
 # In[73]:
+def folder_path(computer):
+    if computer == "work":
+        path = "/home/mwlw3/Documents/Git_class/weather-fun/"
+    elif computer == "home":
+        path = "C:\\Users\\Michelle\\OneDrive\\Documents\\Uni\\MRes\\Git_class\\weather-fun\\"
+    return path
 
+path = folder_path("home")
+df = pd.read_csv("{}weather-raw.csv".format(path), header=None, index_col=0, names=data_labels)
 
-df = pd.read_csv("/home/mwlw3/Documents/Git_class/weather-fun/weather-raw.csv", header=None, index_col=0, names=data_labels)
-
-
-# In[74]:
-
-
-df
-
-
-# In[62]:
-
-
-df['temp'] = df['temp_10'] / 10
 
 
 # In[76]:
 
+cols = []
 
 for i in range(len(df.columns)):
     factor = re.findall("\d+", df.columns[i])
     col_name = df.columns[i]
     if factor:
         factor = int(factor[0])
-        df[col_name+"_new"] = df[col_name] / factor
-        df.drop(labels=col_name, axis='columns')
-
-
+        cols.append(col_name)
+        var_name = re.split("_1", col_name)[0]
+        df[var_name] = df[col_name] / factor
+df.drop(columns=cols, inplace=True)
 # In[77]:
+#df.drop("temp_10", axis=1, inplace=True)
 
+df.columns
 
-df
-
-
-# In[46]:
-
-
-name
-
-
-# In[68]:
-
-
-int(factor)
 
